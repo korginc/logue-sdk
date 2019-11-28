aa8e3a4455ddf52235e53dcef0496563a0286829
Etienne Noreau-Hebert <etienne@korg.co.jp>
2019-11-28 17:07:18 +0900

    [doc] Add doxygen config files.

---
+++ platform/minilogue-xd/docs/doxy.h
@@ -0,0 +1,8 @@
+/** \mainpage minilogue xd SDK documentation
+ *
+ *
+ * \section overview_sec Overview
+ * 
+ * See the Modules and Classes tabs for a breakdown of the SDKs components.
+ *
+ */
---
+++ platform/nutekt-digital/docs/doxy.h
@@ -0,0 +1,8 @@
+/** \mainpage Nu:Tekt NTS-1 digital SDK documentation
+ *
+ *
+ * \section overview_sec Overview
+ * 
+ * See the Modules and Classes tabs for a breakdown of the SDKs components.
+ *
+ */
