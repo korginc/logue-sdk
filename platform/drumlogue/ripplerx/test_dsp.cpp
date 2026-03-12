5b04903833a62cbf9abd8c09a22c6658fee6cbfb
Federico Gennari <federico.gennari@gmail.com>
2026-03-12 14:59:22 +0100

    UT now pass -HW still silent

--- platform/drumlogue/ripplerx/test_dsp.cpp
+++ platform/drumlogue/ripplerx/test_dsp.cpp
@@ -5,7 +5,7 @@
 #include <cassert>
 
 // ==============================================================================
-// instructions (NOTE do not remove these lines!!)
+// instructions fro Windows/WSL (NOTE do not remove these lines!!)
 // /mnt/d/Fede/drumlogue/arm-unknown-linux-gnueabihf/bin/arm-unknown-linux-gnueabihf-g++ -static -std=c++17 -O3 -I.. -I. -I../../common -I../common -DRUNTIME_COMMON_H_ test_dsp.cpp -o run_test && qemu-arm ./run_test | tee run_test_result.log
 // ==============================================================================
 
